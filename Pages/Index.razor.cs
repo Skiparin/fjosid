817160cf2f12eeed0cefba8fb41555cb0c0e62e0
Skiparin <Og1806x9@hotmail.com>
2023-11-26 21:14:34 +0100

    first commit

---
+++ Pages/Index.razor.cs
@@ -0,0 +1,17 @@
+﻿using MudBlazor;
+
+namespace Fjosid.Pages
+{
+    public partial class Index
+    {
+        private bool arrows = true;
+        private bool bullets = true;
+        private bool enableSwipeGesture = true;
+        private bool autocycle = true;
+        private Transition transition = Transition.Fade;
+
+        //"images/img1",
+        //    "images/img2",
+        //    "images/img3",
+    }
+}
