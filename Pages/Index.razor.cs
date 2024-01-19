2034a646b5b0df96a6c05e3c6501479912d132e7
Ørvur Guttesen <ogu@formula.fo>
2024-01-19 13:05:07 +0000

    Latest commit

--- Pages/Index.razor.cs
+++ Pages/Index.razor.cs
@@ -1,17 +1,50 @@
-﻿using MudBlazor;
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using MudBlazor;
+using System.Text.RegularExpressions;
 
 namespace Fjosid.Pages
 {
     public partial class Index
     {
+        [Inject] IJSRuntime JSRuntime { get; set; } = default!;
+
         private bool arrows = true;
         private bool bullets = true;
         private bool enableSwipeGesture = true;
         private bool autocycle = true;
         private Transition transition = Transition.Fade;
 
-        //"images/img1",
-        //    "images/img2",
-        //    "images/img3",
+
+        bool success;
+        string[] errors = { };
+        MudTextField<string> pwField1;
+        MudForm form;
+
+        private IEnumerable<string> NumberOfGuests(string text)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                yield return "Password is required!";
+                yield break;
+            }
+
+            int num = 0;
+            if(Int32.TryParse(text, out num) == false)
+            {
+                yield return "Number of guests must be a number";
+            }
+
+            if (num < 8)
+                yield return "Number of guests must be 8 or higher";
+            if (num > 40)
+                yield return "Number of guests must be 40 or lower";
+        }
+
+
+        private async Task ScrollToBooking()
+        {
+            await JSRuntime.InvokeVoidAsync("scrollToBooking");
+        }
     }
 }
