7e4894dcb92af4068678b8b9f383c000b5df1d94
Skiparin <Og1806x9@hotmail.com>
2024-02-18 17:07:11 +0000

    First design done

--- Program.cs
+++ Program.cs
@@ -13,7 +13,12 @@ namespace Fjosid
 
             // Add services to the container.
             builder.Services.AddRazorPages();
-            builder.Services.AddServerSideBlazor();
+            builder.Services.AddServerSideBlazor()
+                .AddHubOptions(options =>
+            {
+                options.ClientTimeoutInterval = TimeSpan.FromSeconds(60);
+                options.HandshakeTimeout = TimeSpan.FromSeconds(30);
+            }); ;
             builder.Services.AddSingleton<WeatherForecastService>();
 
             builder.Services.AddMudServices();
