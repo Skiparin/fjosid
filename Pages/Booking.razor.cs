81732e858ca07292631927fcb2ab9fa56db2b358
Skiparin <Og1806x9@hotmail.com>
2024-02-24 23:49:16 +0000

    Moved booking into its own page, added social media icons and language button

---
+++ Pages/Booking.razor.cs
@@ -0,0 +1,33 @@
+﻿using MudBlazor;
+using System.Text.RegularExpressions;
+
+namespace Fjosid.Pages
+{
+    public partial class Booking
+    {
+        bool success;
+        string[] errors = { };
+        MudTextField<string> pwField1;
+        MudForm form;
+
+        private IEnumerable<string> NumberOfGuests(string text)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                yield return "Password is required!";
+                yield break;
+            }
+
+            int num = 0;
+            if (Int32.TryParse(text, out num) == false)
+            {
+                yield return "Number of guests must be a number";
+            }
+
+            if (num < 8)
+                yield return "Number of guests must be 8 or higher";
+            if (num > 40)
+                yield return "Number of guests must be 40 or lower";
+        }
+    }
+}
--- Pages/Index.razor.cs
+++ Pages/Index.razor.cs
@@ -7,6 +7,9 @@ namespace Fjosid.Pages
 {
     public partial class Index
     {
+        [CascadingParameter]
+        public int LanguageId { get; set; } = 1;
+
         [Inject] IJSRuntime JSRuntime { get; set; } = default!;
 
         private bool arrows = true;
@@ -14,32 +17,6 @@ namespace Fjosid.Pages
         private bool enableSwipeGesture = true;
         private bool autocycle = true;
         private Transition transition = Transition.Fade;
-
-
-        bool success;
-        string[] errors = { };
-        MudTextField<string> pwField1;
-        MudForm form;
-
-        private IEnumerable<string> NumberOfGuests(string text)
-        {
-            if (string.IsNullOrWhiteSpace(text))
-            {
-                yield return "Password is required!";
-                yield break;
-            }
-
-            int num = 0;
-            if(Int32.TryParse(text, out num) == false)
-            {
-                yield return "Number of guests must be a number";
-            }
-
-            if (num < 8)
-                yield return "Number of guests must be 8 or higher";
-            if (num > 40)
-                yield return "Number of guests must be 40 or lower";
-        }
 
 
         private async Task ScrollToBooking()
